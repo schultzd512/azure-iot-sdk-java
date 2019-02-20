7b58c12d122902a0147303fe4b0e467fdd649504
timtay-microsoft <timtay@microsoft.com>
2019-02-20 12:44:24 -0800

    split up a into smaller subsets

--- iot-e2e-tests/android/app/src/androidTest/java/com/microsoft/azure/sdk/iot/android/iothubservices/errorinjection/methods/DeviceMethodErrInjDeviceAndroidRunner.java
+++ iot-e2e-tests/android/app/src/androidTest/java/com/microsoft/azure/sdk/iot/android/iothubservices/errorinjection/methods/DeviceMethodErrInjDeviceAndroidRunner.java
@@ -9,6 +9,7 @@ import com.microsoft.appcenter.espresso.Factory;
 import com.microsoft.appcenter.espresso.ReportHelper;
 import com.microsoft.azure.sdk.iot.android.BuildConfig;
 import com.microsoft.azure.sdk.iot.android.helper.TestGroupA;
+import com.microsoft.azure.sdk.iot.android.helper.TestGroupB;
 import com.microsoft.azure.sdk.iot.common.helpers.ClientType;
 import com.microsoft.azure.sdk.iot.common.helpers.DeviceTestManager;
 import com.microsoft.azure.sdk.iot.common.helpers.Rerun;
@@ -24,7 +25,7 @@ import org.junit.runners.Parameterized;
 import java.util.ArrayList;
 import java.util.Collection;
 
-@TestGroupA
+@TestGroupB
 @RunWith(Parameterized.class)
 public class DeviceMethodErrInjDeviceAndroidRunner extends DeviceMethodErrInjTests
 {
--- iot-e2e-tests/android/app/src/androidTest/java/com/microsoft/azure/sdk/iot/android/iothubservices/messaging/SendMessagesModuleAndroidRunner.java
+++ iot-e2e-tests/android/app/src/androidTest/java/com/microsoft/azure/sdk/iot/android/iothubservices/messaging/SendMessagesModuleAndroidRunner.java
@@ -9,6 +9,7 @@ import com.microsoft.appcenter.espresso.Factory;
 import com.microsoft.appcenter.espresso.ReportHelper;
 import com.microsoft.azure.sdk.iot.android.BuildConfig;
 import com.microsoft.azure.sdk.iot.android.helper.TestGroupA;
+import com.microsoft.azure.sdk.iot.android.helper.TestGroupC;
 import com.microsoft.azure.sdk.iot.common.helpers.ClientType;
 import com.microsoft.azure.sdk.iot.common.helpers.Rerun;
 import com.microsoft.azure.sdk.iot.common.tests.iothubservices.telemetry.SendMessagesTests;
@@ -23,7 +24,7 @@ import org.junit.runners.Parameterized;
 
 import java.util.Collection;
 
-@TestGroupA
+@TestGroupC
 @RunWith(Parameterized.class)
 public class SendMessagesModuleAndroidRunner extends SendMessagesTests
 {
